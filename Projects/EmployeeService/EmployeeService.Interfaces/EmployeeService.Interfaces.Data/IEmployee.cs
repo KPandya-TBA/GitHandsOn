7366302f45b8fb994496f9c6293676fc318f9e99
Komal Pandya <kpandya@thebeastapps.com>
2016-11-24 19:11:58 +0530

    Employee WebService.

---
+++ Projects/EmployeeService/EmployeeService.Interfaces/EmployeeService.Interfaces.Data/IEmployee.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeService.Interfaces.EmployeeService.Interfaces.Data
+{
+    public interface IEmployee
+    {
+        /// <summary>
+        /// Inserts Employee.
+        /// </summary>
+        void InsertEmployee();
+
+        /// <summary>
+        /// Retrieve Employees.
+        /// </summary>
+        /// <returns></returns>
+        DataTable RetrieveEmployee();
+
+        void UpdateEmployee ()
+    }
+}
